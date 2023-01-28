4d62bb1f047627007f5f3e80142e5504bc3d64b3
samchencode <samchencode@gmail.com>
2023-01-28 07:05:18 -0500

    build: config for render

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,19 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
     connection: {
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      schema: env("DATABASE_SCHEMA", "public"),
-      ssl: {
-        rejectUnauthorized: false,
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password
       },
+      debug: false,
     },
-  },
-});
+  };
+};
