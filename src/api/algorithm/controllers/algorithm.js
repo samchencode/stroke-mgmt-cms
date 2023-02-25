598ac0442fc05a6bb7af238b4d175d7145b55c88
samchencode <samchencode@gmail.com>
2023-02-25 07:25:59 -0500

    feat: algorithm markdown to html

--- src/api/algorithm/controllers/algorithm.js
+++ src/api/algorithm/controllers/algorithm.js
@@ -6,4 +6,20 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::algorithm.algorithm');
+const StrapiMarkdown = require("../../../lib/md.js");
+const model = require("../content-types/algorithm/schema.json");
+
+const { md } = new StrapiMarkdown(model);
+
+// FIXME: md doesn't work with component fields like outcomes and switches yet
+
+module.exports = createCoreController('api::algorithm.algorithm', () => ({
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+    return { data: await md(data), meta };
+  },
+  async findOne(ctx) {
+    const { data, meta } = await super.findOne(ctx);
+    return { data: await md(data), meta };
+  },
+}));
